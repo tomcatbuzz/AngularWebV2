68a787a7e1bf213ff9e0123de2d59257941fa1fd
tomcatbuzz <tomcatbuzz@yahoo.com>
2019-06-15 23:14:44 -0400

    working

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([{ path: '', component: HomeComponent }])
   ]
 })
 export class HomeModule { }
--- src/app/portfolio/portfolio.module.ts
+++ src/app/portfolio/portfolio.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { PortfolioComponent } from './portfolio.component';
 
 @NgModule({
   declarations: [PortfolioComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([{ path: '', component: PortfolioComponent }])
   ]
 })
 export class PortfolioModule { }
